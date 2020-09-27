67b71a4576c30eb05984f850580cda625d7e604b
beomju <20171643.info@edu.hanbat.ac.kr>
2020-09-27 19:34:57 +0900

    아이디, 비밀번호 찾기 api호출 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,6 @@
 // 로그인, 회원 가입, (ex) 회원 탈퇴
 import { instance } from './index';
+import { posts } from './index';
 
 // 회원가입 API
 function registerUser(userData) {
@@ -11,27 +12,39 @@ function loginUser(userData) {
   return instance.post('v1/auth/login', userData);
 }
 
+//아이디 찾기 API
+function findUserId(userData) {
+  return instance.post('v1/auth/get_owner_id', userData);
+}
+
+//비밀번호 찾기 API
+function findUserPassword(userData) {
+  return instance.patch('v1/auth/update_random_password', userData);
+}
+
 //회원정보관리 API
 function fetchUserInfo(userId) {
-  return instance.get(`v1/owner/owner_info/${userId}`);
+  return posts.get(`v1/owner/owner_info/${userId}`);
 }
 
 function updateUser(userData) {
-  return instance.patch('v1/owner/update_owner_info', userData);
+  return posts.patch('v1/owner/update_owner_info', userData);
 }
 
 function updateUserPassword(userData) {
-  return instance.patch('v1/owner/update_owner_password', userData);
+  return posts.patch('v1/owner/update_owner_password', userData);
 }
 
 //회원탈퇴 API
 function deleteUser(userId) {
-  return instance.delete(`v1/owner/delete_owner/${userId}`);
+  return posts.delete(`v1/owner/delete_owner/${userId}`);
 }
 
 export {
   registerUser,
   loginUser,
+  findUserId,
+  findUserPassword,
   fetchUserInfo,
   updateUser,
   updateUserPassword,
