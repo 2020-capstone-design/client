80c37c99e1762f88a9d4b17382f645b827ea0203
beomju <20171643.info@edu.hanbat.ac.kr>
2020-10-12 21:27:09 +0900

    Sample Id, Password with Alert

---
+++ src/components/Sample.vue
@@ -0,0 +1,32 @@
+<template>
+  <button id="sample-btn" @click="alert">샘플용 아이디, 비밀번호</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    alert() {
+      alert('샘플용 아이디 : test, 비밀번호 : test@1');
+    },
+  },
+};
+</script>
+
+<style>
+#sample-btn {
+  position: relative;
+  left: 50%;
+  margin-top: 20px;
+  transform: translateX(-50%);
+  background-position: left;
+  background-size: 200%;
+  color: white;
+  background: #9ab4d4;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.4s;
+  display: inline;
+}
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,22 +2,24 @@
   <div>
     <h1 class="page-header">로그인</h1>
     <LoginForm></LoginForm>
-    <p id="login-page-header">로그인 후 사용 가능한 서비스입니다.</p>
+    <p id="login-page-text">로그인 후 사용 가능한 서비스입니다.</p>
+    <Sample></Sample>
   </div>
 </template>
 
 <script>
 import LoginForm from '@/components/LoginForm.vue';
-
+import Sample from '@/components/Sample.vue';
 export default {
   components: {
     LoginForm,
+    Sample,
   },
 };
 </script>
 
 <style>
-#login-page-header {
+#login-page-text {
   text-align: center;
   color: #364f6b;
 }
